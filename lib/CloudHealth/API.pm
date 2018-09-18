8348097628dd42d63d148cb3cf55b21e5636c81d
Jose Luis Martinez <jlmartinez@capside.com>
2018-09-18 13:38:36 +0000

    Implement CreatePartnerCustomer and DeleteExistingCustomer

--- lib/CloudHealth/API.pm
+++ lib/CloudHealth/API.pm
@@ -486,6 +486,47 @@ package CloudHealth::API::Call::UpdateTagsForSingleAsset;
   sub _method { 'POST' }
   sub _url { 'https://chapi.cloudhealthtech.com/v1/custom_tags' }
 
+package CloudHealth::API::Call::CreatePartnerCustomer;
+  use Moo;
+  use MooX::StrictConstructor;
+  use Types::Standard qw/Int Str Dict Maybe ArrayRef/;
+
+  has name => (is => 'ro', isa => Str, required => 1);
+  has address => (is => 'ro', isa => Dict[street1 => Str, street2 => Str, city => Str, state => Str, zipcode => Int, country => Str], required => 1);
+  has classification => (is => 'ro', isa => Str);
+  has trial_expiration_date => (is => 'ro', isa => Str);
+  has billing_contact => (is => 'ro', isa => Str);
+  has partner_billing_configuration => (is => 'ro', isa => Dict[enabled => Str, folder => Maybe[Str]]);
+  has tags => (is => 'ro', isa => ArrayRef[Dict[key => Str, value => Str]]);
+
+  sub _body_params { [
+    { name => 'name' },
+    { name => 'address' },
+    { name => 'classification' },
+    { name => 'trial_expiration_date' },
+    { name => 'billing_contact' },
+    { name => 'partner_billing_configuration' },
+    { name => 'tags' },
+  ] }
+  sub _query_params { [ ] }
+  sub _url_params { [ ] }
+  sub _method { 'POST' }
+  sub _url { 'https://chapi.cloudhealthtech.com/v1/customers' }
+
+package CloudHealth::API::Call::DeleteExistingCustomer;
+  use Moo;
+  use MooX::StrictConstructor;
+  use Types::Standard qw/Int/;
+
+  has customer_id => (is => 'ro', isa => Int, required => 1);
+
+  sub _query_params { [ ] }
+  sub _url_params { [
+    { name => 'customer_id' },
+  ] }
+  sub _method { 'DELETE' }
+  sub _url { 'https://chapi.cloudhealthtech.com/v1/customers/:customer_id' }
+
 package CloudHealth::API::Call::GetSingleCustomer;
   use Moo;
   use MooX::StrictConstructor;
@@ -730,9 +771,15 @@ package CloudHealth::API;
 
   sub SpecificCustomerReport { die "TODO" }
   sub AssetsForSpecificCustomer { die "TODO" }
-  sub CreatePartnerCustomer { die "TODO" }
+  sub CreatePartnerCustomer {
+    my $self = shift;
+    $self->_invoke('CreatePartnerCustomer', [ @_ ]);  
+  }
   sub ModifyExistingCustomer { die "TODO" }
-  sub DeleteExistingCustomer { die "TODO" }
+  sub DeleteExistingCustomer {
+    my $self = shift;
+    $self->_invoke('DeleteExistingCustomer', [ @_ ]);  
+  }
   sub GetSingleCustomer {
     my $self = shift;
     $self->_invoke('GetSingleCustomer', [ @_ ]);
