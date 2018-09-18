443dc1a05c65972c7532b658e81367a662879ba9
Jose Luis Martinez <jlmartinez@capside.com>
2018-09-18 07:56:39 +0000

    Correct and test UpdateTagsForSingleAsset

--- lib/CloudHealth/API.pm
+++ lib/CloudHealth/API.pm
@@ -472,10 +472,15 @@ package CloudHealth::API::Call::MetricsForSingleAsset;
 package CloudHealth::API::Call::UpdateTagsForSingleAsset;
   use Moo;
   use MooX::StrictConstructor;
-  use Types::Standard qw/HashRef/;
+  use Types::Standard qw/Dict Str ArrayRef Int/;
 
-  has document => (is => 'ro', isa => HashRef, required => 1);
+  our $tags_cons = Dict[key => Str, value => Str];
+  our $tag_group_cons = Dict[asset_type => Str, ids => ArrayRef[Int], tags => ArrayRef[$tags_cons]];
+  has tag_groups => (is => 'ro', isa => ArrayRef[$tag_group_cons], required => 1);
 
+  sub _body_params { [
+    { name => 'tag_groups' },
+  ] }
   sub _query_params { [ ] }
   sub _url_params { [ ] }
   sub _method { 'POST' }
