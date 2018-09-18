a2db7cb24c7a02cd0f5d769e2ba18acea4c0cf2a
Jose Luis Martinez <jlmartinez@capside.com>
2018-09-18 13:45:43 +0000

    Implemennt ModifyExistingCustomer

--- lib/CloudHealth/API.pm
+++ lib/CloudHealth/API.pm
@@ -513,6 +513,37 @@ package CloudHealth::API::Call::CreatePartnerCustomer;
   sub _method { 'POST' }
   sub _url { 'https://chapi.cloudhealthtech.com/v1/customers' }
 
+package CloudHealth::API::Call::ModifyExistingCustomer;
+  use Moo;
+  use MooX::StrictConstructor;
+  use Types::Standard qw/Int Str Dict Maybe ArrayRef/;
+
+  has customer_id => (is => 'ro', isa => Int, required => 1);
+
+  has name => (is => 'ro', isa => Str);
+  has address => (is => 'ro', isa => Dict[street1 => Str, street2 => Str, city => Str, state => Str, zipcode => Int, country => Str]);
+  has classification => (is => 'ro', isa => Str);
+  has trial_expiration_date => (is => 'ro', isa => Str);
+  has billing_contact => (is => 'ro', isa => Str);
+  has partner_billing_configuration => (is => 'ro', isa => Dict[enabled => Str, folder => Maybe[Str]]);
+  has tags => (is => 'ro', isa => ArrayRef[Dict[key => Str, value => Str]]);
+
+  sub _body_params { [
+    { name => 'name' },
+    { name => 'address' },
+    { name => 'classification' },
+    { name => 'trial_expiration_date' },
+    { name => 'billing_contact' },
+    { name => 'partner_billing_configuration' },
+    { name => 'tags' },
+  ] }
+  sub _query_params { [ ] }
+  sub _url_params { [
+    { name => 'customer_id' }, 
+  ] }
+  sub _method { 'PUT' }
+  sub _url { 'https://chapi.cloudhealthtech.com/v1/customers/:customer_id' }
+
 package CloudHealth::API::Call::DeleteExistingCustomer;
   use Moo;
   use MooX::StrictConstructor;
@@ -775,7 +806,10 @@ package CloudHealth::API;
     my $self = shift;
     $self->_invoke('CreatePartnerCustomer', [ @_ ]);  
   }
-  sub ModifyExistingCustomer { die "TODO" }
+  sub ModifyExistingCustomer {
+    my $self = shift;
+    $self->_invoke('ModifyExistingCustomer', [ @_ ]);  
+  }
   sub DeleteExistingCustomer {
     my $self = shift;
     $self->_invoke('DeleteExistingCustomer', [ @_ ]);  
