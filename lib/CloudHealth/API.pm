a0f63e7bbc663aaef5457e28c8e36d0a098c43b5
Jose Luis Martinez <jlmartinez@capside.com>
2018-09-18 16:32:53 +0000

    Implement SpecificCustomerReport and AssetsForSpecificCustomer

--- lib/CloudHealth/API.pm
+++ lib/CloudHealth/API.pm
@@ -486,6 +486,43 @@ package CloudHealth::API::Call::UpdateTagsForSingleAsset;
   sub _method { 'POST' }
   sub _url { 'https://chapi.cloudhealthtech.com/v1/custom_tags' }
 
+package CloudHealth::API::Call::SpecificCustomerReport;
+  use Moo;
+  use MooX::StrictConstructor;
+  use Types::Standard qw/Int Str/;
+
+  has report_type => (is => 'ro', isa => Str, required => 1);
+  has report_id => (is => 'ro', isa => Str, required => 1);
+  has client_api_id => (is => 'ro', isa => Int, required => 1);
+
+  sub _query_params { [
+    { name => 'client_api_id' },  
+  ] }
+  sub _url_params { [
+    { name => 'report_type', location => 'report-type' },
+    { name => 'report_id', location => 'report-id' },
+  ] }
+  sub _method { 'GET' }
+  sub _url { 'https://chapi.cloudhealthtech.com/olap_reports/:report-type/:report-id' }
+
+package CloudHealth::API::Call::AssetsForSpecificCustomer;
+  use Moo;
+  use MooX::StrictConstructor;
+  use Types::Standard qw/Int Str/;
+
+  has client_api_id => (is => 'ro', isa => Int, required => 1);
+  has api_version => (is => 'ro', isa => Int, default => 2);
+  has name => (is => 'ro', isa => Str, required => 1);
+
+  sub _query_params { [
+    { name => 'client_api_id' },  
+    { name => 'api_version' },  
+    { name => 'name' },  
+  ] }
+  sub _url_params { [ ] }
+  sub _method { 'GET' }
+  sub _url { 'https://chapi.cloudhealthtech.com/api/search.json' }
+
 package CloudHealth::API::Call::CreatePartnerCustomer;
   use Moo;
   use MooX::StrictConstructor;
@@ -915,8 +952,14 @@ package CloudHealth::API;
     $self->_invoke('UpdateTagsForSingleAsset', [ @_ ]);
   }
 
-  sub SpecificCustomerReport { die "TODO" }
-  sub AssetsForSpecificCustomer { die "TODO" }
+  sub SpecificCustomerReport {
+    my $self = shift;
+    $self->_invoke('SpecificCustomerReport', [ @_ ]); 
+  }
+  sub AssetsForSpecificCustomer {
+    my $self = shift;
+    $self->_invoke('AssetsForSpecificCustomer', [ @_ ]);
+  }
   sub CreatePartnerCustomer {
     my $self = shift;
     $self->_invoke('CreatePartnerCustomer', [ @_ ]);  
