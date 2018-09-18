325aabc80f24a89909ab5628dcc1f09636c385e9
Jose Luis Martinez <jlmartinez@capside.com>
2018-09-18 10:38:17 +0000

    Implement GetSingleCustomer and GetAllCustomers

--- lib/CloudHealth/API.pm
+++ lib/CloudHealth/API.pm
@@ -486,6 +486,30 @@ package CloudHealth::API::Call::UpdateTagsForSingleAsset;
   sub _method { 'POST' }
   sub _url { 'https://chapi.cloudhealthtech.com/v1/custom_tags' }
 
+package CloudHealth::API::Call::GetSingleCustomer;
+  use Moo;
+  use MooX::StrictConstructor;
+  use Types::Standard qw/Int/;
+
+  has customer_id => (is => 'ro', isa => Int, required => 1);
+
+  sub _query_params { [ ] }
+  sub _url_params { [
+    { name => 'customer_id' },
+  ] }
+  sub _method { 'GET' }
+  sub _url { 'https://chapi.cloudhealthtech.com/v1/customers/:customer_id' }
+
+
+package CloudHealth::API::Call::GetAllCustomers;
+  use Moo;
+  use MooX::StrictConstructor;
+
+  sub _query_params { [ ] }
+  sub _url_params { [ ] }
+  sub _method { 'GET' }
+  sub _url { 'https://chapi.cloudhealthtech.com/v1/customers' }
+
 
 package CloudHealth::API::Caller;
   use Moo;
@@ -709,8 +733,14 @@ package CloudHealth::API;
   sub CreatePartnerCustomer { die "TODO" }
   sub ModifyExistingCustomer { die "TODO" }
   sub DeleteExistingCustomer { die "TODO" }
-  sub GetSingleCustomer { die "TODO" }
-  sub GetAllCustomers { die "TODO" }
+  sub GetSingleCustomer {
+    my $self = shift;
+    $self->_invoke('GetSingleCustomer', [ @_ ]);
+  }
+  sub GetAllCustomers {
+    my $self = shift;
+    $self->_invoke('GetAllCustomers', [ @_ ]); 
+  }
   sub StatementForSingleCustomer { die "TODO" }
   sub StatementsForAllCustomers { die "TODO" }
 
