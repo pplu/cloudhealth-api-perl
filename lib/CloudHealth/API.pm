32d53d9c0048427dada5ac147c9877cab7c30e5a
Jose Luis Martinez <jlmartinez@capside.com>
2018-12-10 12:56:29 +0000

    Extract Error classes

--- lib/CloudHealth/API.pm
+++ lib/CloudHealth/API.pm
@@ -1,39 +1,3 @@
-package CloudHealth::API::Error;
-  use Moo;
-  use Types::Standard qw/Str/;
-  extends 'Throwable::Error';
-
-  has type => (is => 'ro', isa => Str, required => 1);
-  has detail => (is => 'ro');
-
-  sub header {
-    my $self = shift;
-    return sprintf "Exception with type: %s: %s", $self->type, $self->message;
-  }
-
-  sub as_string {
-    my $self = shift;
-    if (defined $self->detail) {
-      return sprintf "%s\nDetail: %s", $self->header, $self->detail;
-    } else {
-      return $self->header;
-    }
-  }
-
-package CloudHealth::API::RemoteError;
-  use Moo;
-  use Types::Standard qw/Int/;
-  extends 'CloudHealth::API::Error';
-
-  has '+type' => (default => sub { 'Remote' });
-  has status => (is => 'ro', isa => Int, required => 1);
-
-  around header => sub {
-    my ($orig, $self) = @_;
-    my $orig_message = $self->$orig;
-    sprintf "%s with HTTP status %d", $orig_message, $self->status;
-  };
-
 package CloudHealth::API::Credentials;
   use Moo;
   use Types::Standard qw/Maybe Str Bool/;
@@ -76,6 +40,7 @@ package CloudHealth::API::CallObjectFormer;
   use Moo;
   use HTTP::Tiny;
   use JSON::MaybeXS;
+  use CloudHealth::API::Error;
 
   has _json => (is => 'ro', default => sub { JSON::MaybeXS->new });
 
